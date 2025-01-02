bcefaea9e64ef23ee9e918488138846bc3ebec1c
vulpaw <karolaleturecki11@gmail.com>
2025-01-02 21:04:58 +0100

    wrqa

--- my-github-pages/assets/js/main.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('.button');
-
-    buttons.forEach(button => {
-        button.addEventListener('click', function() {
-            alert(`You clicked on ${this.innerText}`);
-        });
-    });
-});
